cf18db91cd790c99328a64454380e4438cbf8a84
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-06-13 00:42:27 +0200

    Fix compile error
    Change-Id: I9a19ec81b9e4768cf5a18d467d3efde34561e2f1

--- src/com/android/incallui/ProximitySensor.java
+++ src/com/android/incallui/ProximitySensor.java
@@ -64,7 +64,6 @@ public class ProximitySensor implements AccelerometerListener.ChangeListener,
     private boolean mDialpadVisible;
     private Context mContext;
 
-    private Context mContext;
     private final Handler mHandler = new Handler();
     private final Runnable mActivateSpeaker = new Runnable() {
         @Override
